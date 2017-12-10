d62c989ffd847880153f79bfcff7f5cbc2950e4e
Julie Moon <8juliemoon@gmail.com>
2017-12-10 12:27:45 -0500

    add Layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layout/Layout';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Layout>
+          <p>wrapping inside of layout</p>
+        </Layout>
       </div>
     );
   }
