9f669dc1ef67f005a712301a3fa9cedef887f325
Julie Moon <8juliemoon@gmail.com>
2017-12-10 19:08:10 -0500

    add styles to buttons

--- my-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ my-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,13 +1,16 @@
 // inside of BuildControls folder >BuildControl folder
+// build all of this inside of BuildControls!
 
 import React from 'react'
+import classes from './BuildControl.css';
+
 
 const buildControl = (props) =>{
-  <div>
+  <div className={classes.BuildControl}>
     {/* layout will be name of ingredient and two buttons add and remove */}
-    <div>{props.label}</div>
-    <button>Less</button>
-    <button>More</button>
+    <div className={classes.Label}>{props.label}</div>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More}>More</button>
   </div>
 }
 
