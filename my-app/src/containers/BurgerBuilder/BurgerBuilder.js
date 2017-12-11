8bb449a4db9bc29bfd799047fa61f7e9d2cb9b73
Julie Moon <8juliemoon@gmail.com>
2017-12-10 22:33:52 -0500

    navigation styled and added

--- my-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,6 +75,15 @@ class BurgerBuilder extends Component{
   purchaseHandler = () => {
     this.setState({purchasing:true});
   }
+
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
+  purchaseContinueHandler = () => {
+    alert('You continue!');
+  }
+
   render(){
     const disabledInfo = {
       ...this.state.ingredients
@@ -86,8 +95,12 @@ class BurgerBuilder extends Component{
    
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
-          <OrderSummary ingredients={this.state.ingredients}/>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+          <OrderSummary 
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}/>
         </Modal>
         {/* replace this fake Burger div with real Burger component */}
         {/* <div>Burger</div> */}
